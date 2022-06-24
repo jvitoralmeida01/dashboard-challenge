03d5e04ba561bc51c89fa9937f32815d1e05d2c2
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2022-06-24 09:58:52 -0300

    image, inputs and button on login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,8 +1,9 @@
 import React, { ReactElement } from 'react';
 import {
-  Button, Flex, Text
+  Button, Flex, Heading, Image, Spacer, VStack
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import FormInput from '../Components/FormInput';
 
 function LoginPage() : ReactElement {
   const navigate = useNavigate();
@@ -10,9 +11,16 @@ function LoginPage() : ReactElement {
   const handleClick = () : void => navigate('/dashboard');
 
   return (
-    <Flex height="100vh" width={['100vw', '80vw', '60vw', '50vw']} align="center" justify="center" background="neutral.100">
-      <Text>Hello duiwa</Text>
-      <Button onClick={handleClick}>Login</Button>
+    <Flex padding={12} height="100vh" width={['100vw', '80vw', '60vw', '50vw']} align="center" justify="center" background="neutral.900">
+      <VStack spacing={8}>
+        <Image src="/logo.svg" alt="logo" width={['50%', '60%', '100%']} />
+        <Heading fontSize="150%" mb={2}>Entrar na plataforma</Heading>
+        <Spacer />
+        <FormInput label="E-mail" type="email" />
+        <FormInput label="Senha" type="password" />
+        <Spacer />
+        <Button onClick={handleClick} variant="primary">Entrar</Button>
+      </VStack>
     </Flex>
   );
 }
