c317768c7b5d9baa75edc0b6df77cae4026cb1ca
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2022-06-28 16:32:56 -0300

    creating wraper cards

---
+++ src/Components/Card.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Box, Container } from '@chakra-ui/react';
+
+const getRoundedByVariant = (variant:string|undefined) : string => {
+  switch (variant) {
+    case 'small': return '15px';
+    case 'medium': return '12px';
+    case 'large': return '20px';
+    default: return '15px';
+  }
+};
+
+const getPaddingByVariant = (variant:string|undefined) : string => {
+  switch (variant) {
+    case 'small': return '25px 16px';
+    case 'medium': return '25px 24px';
+    case 'large': return '55px 40px';
+    default: return '25px 16px';
+  }
+};
+
+type Props = {
+  children?: any,
+  rounded?: string,
+  width?: number,
+  height?: number,
+  minWidth?: number | string,
+  minHeight?: number | string,
+  variant?: 'small' | 'medium' | 'large'
+}
+
+function Card({
+  children, rounded, width, height, minWidth, minHeight, variant
+}: Props) : React.ReactElement {
+  return (
+    <Box
+      bg="neutral.900"
+      w={width}
+      minWidth={minWidth}
+      h={height}
+      minHeight={minHeight}
+      rounded={rounded ?? getRoundedByVariant(variant)}
+      p={getPaddingByVariant(variant)}
+    >
+      {children}
+    </Box>
+  );
+}
+
+export default Card;
