d083e48d04468e4424aa29435fbb0b6ae9241878
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2022-06-28 01:28:49 -0300

    adding cards layout

--- src/Components/Graphs.tsx
+++ src/Components/Graphs.tsx
@@ -1,11 +1,20 @@
-import { Avatar, Box, VStack } from '@chakra-ui/react';
+import {
+  Avatar, Box, Container, HStack, Text, VStack
+} from '@chakra-ui/react';
 import React from 'react';
 import useAuthContext from '../Hooks/useAuthContext';
 
 function Graphs() : React.ReactElement {
   const authContext = useAuthContext();
   return (
-    <div>a</div>
+    <>
+      <Text fontWeight="bold" fontSize="28px" color="neutral.200" mt="18px" mb="18px" ml="28px">Início</Text>
+      <HStack spacing="32px">
+        <Box bg="neutral.900" w={200} h={200} rounded="12px" />
+        <Box bg="neutral.900" w={200} h={200} rounded="12px" />
+        <Box bg="neutral.900" w={200} h={200} rounded="12px" />
+      </HStack>
+    </>
   );
 };
 
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -27,33 +27,21 @@ function DashboardPage() : React.ReactElement {
       <Navbar />
       <Spacer />
       <Grid
-        w="100vw"
+        w="100%"
         templateColumns="repeat(10, 1fr)"
-        gap={4}
       >
-        <GridItem ref={sideItem} colSpan={1} bg="tomato">
+        <GridItem ref={sideItem} colSpan={1}>
           <Sidebar height={height} />
         </GridItem>
-        <GridItem colSpan={9} bg="papayawhip" />
+        <GridItem colSpan={9}>
+          <Graphs />
+          <Graphs />
+          <Graphs />
+          <Graphs />
+        </GridItem>
       </Grid>
     </>
   );
 }
 
 export default DashboardPage;
-
-// eslint-disable-next-line no-lone-blocks
-{ /* <Grid
-        boxSize="100%"
-        templateRows="repeat(20, 1fr)"
-        templateColumns="repeat(20, 1fr)"
-        gap={4}
-      >
-        <GridItem rowSpan={3} colSpan={22} bg="green.200">
-          <Navbar />
-        </GridItem>
-        <GridItem rowSpan={20} colSpan={2} bg="tomato" />
-        <GridItem rowSpan={20} colSpan={20} bg="papayawhip">
-          <Graphs />
-        </GridItem>
-      </Grid> */ }
