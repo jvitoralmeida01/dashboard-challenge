f8dbeb3798175e6ab27fe32a41b99df30ba5ad48
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2022-06-28 00:36:41 -0300

    Some adjustments to the navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,14 +3,13 @@ import {
   Box, Container, Flex, HStack, Image, Stack, Text
 } from '@chakra-ui/react';
 import React from 'react';
-import { NavigationBarItem } from 'typescript';
 import useAuthContext from '../Hooks/useAuthContext';
 import logoImagePath from '../Assets/logo.svg';
 
 function Navbar() : React.ReactElement {
   const authContext = useAuthContext();
   return (
-    <Container minHeight="8vh" minWidth="100vw" bg="neutral.900" sx={{ boxShadow: '0px 3px 6px #00000014' }}>
+    <Container minHeight="8vh" minWidth="100vw" bg="neutral.900" sx={{ boxShadow: '0px 3px 6px #00000014', position: 'sticky', mb: 4 }}>
       <Flex direction="row" justify="space-between" align="center" p={2}>
         <Image h={['60px']} src={logoImagePath} />
         <HStack spacing="13px">
