4998a19f33aa487b6f21a30a2b4f610321240a4d
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2022-06-28 00:54:42 -0300

    adjusting navbar's width to fit the screen

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,7 +9,7 @@ import logoImagePath from '../Assets/logo.svg';
 function Navbar() : React.ReactElement {
   const authContext = useAuthContext();
   return (
-    <Container minHeight="8vh" minWidth="100vw" bg="neutral.900" sx={{ boxShadow: '0px 3px 6px #00000014', position: 'sticky', mb: 4 }}>
+    <Container minHeight="8vh" minWidth="100%" bg="neutral.900" sx={{ boxShadow: '0px 3px 6px #00000014', position: 'sticky', mb: 4 }}>
       <Flex direction="row" justify="space-between" align="center" p={2}>
         <Image h={['60px']} src={logoImagePath} />
         <HStack spacing="13px">
